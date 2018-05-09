44928bec681d7c1bd5ea9af66d68137cb4cf1dee
Navdeep Sandhu <navdeep.usict@gmail.com password = navdeep1>
2018-05-09 21:41:26 +0530

    Changed Implementation of Aloneinarray

--- src/BitManipulation/Pep_JavaIP_15BitManipulation_480AloneinArray.java
+++ src/BitManipulation/Pep_JavaIP_15BitManipulation_480AloneinArray.java
@@ -1,27 +1,43 @@
 package BitManipulation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Pep_JavaIP_15BitManipulation_480AloneinArray {
 
+
 	public static void main(String[] args) {
-		Scanner scn=new Scanner(System.in);
-		int num=scn.nextInt();
-		int[] arr=new int[num];
-		int sum=0;
-		
-		for(int i=0; i<num; i++) {
-			arr[i]=scn.nextInt();
+		Scanner scn = new Scanner(System.in);
+		int num = scn.nextInt();
+		int[] arr = new int[num];
+
+		for (int i = 0; i < num; i++) {
+			arr[i] = scn.nextInt();
 		}
-		int nor=arr[0];
-		for(int i=1; i<num; i++) {
-			
-			nor|=arr[i];
-			nor=~(nor);
+		HashMap<Integer,Boolean> map=new HashMap<>();
+		int sum = 0;
+		for (int i = 0; i < num; i++) {
+			sum += (arr[i]);
+			map.put(arr[i],true);
+		}
+		 int sumofuniqueelements = 0;
+		ArrayList<Integer> al =new ArrayList<>(map.keySet());
+		for(int val: al) {
+			 sumofuniqueelements+= val;
 		}
 		
-		System.out.println(~nor);
-		
+		int uniquenum=3*sumofuniqueelements-(sum);
+		System.out.println(uniquenum/2);
+
 	}
+	
+	
+/*	 twos = twos | (ones & arr[i]);
+     ones = ones ^ arr[i];
+     common_bit_mask = ~(ones & twos);
+     ones &= common_bit_mask;
+     twos &= common_bit_mask;*/
 
 }
